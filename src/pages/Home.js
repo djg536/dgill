20a78c6c7470a8a0395cd1cd8ea99d07ed3daef5
Dave <djg536@york.ac.uk>
2019-11-03 18:19:30 +0000

    Added particles.js particles to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+//TODO: include license for particles.js and react-particles-js!
 
 class Home extends Component {
   render() {
     return (
         <div>
           <h1>You can call me Dave.</h1>
+          <Particles />
         </div>
     );
   }
 }
+
 
 export default Home;
